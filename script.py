98a63acf77f97be47ffa8e7f4ab757186f98488b
Ulises <ulises.vr.rm@gmail.com>
2018-12-15 12:52:13 +0000

    Simple example working

---
+++ script.py
@@ -0,0 +1,16 @@
+
+# Library imports
+import json 
+from pprint  import pprint
+import random
+
+# We get the json file content (phrasal verbs)
+with open('phrasal_verbs.json') as dataFile:
+    phrasalVerbs = json.load(dataFile)
+
+# We get a random position to get the daily phrasal verb
+for x in range(len(phrasalVerbs)):
+    randomPosition = random.randint(0,len(phrasalVerbs))
+
+# Now, we have the choosen item
+print(phrasalVerbs[randomPosition])
