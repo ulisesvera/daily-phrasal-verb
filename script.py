e9a0d7b43c852b42b7503b51e0ca7467dd341338
Ulises <ulises.vr.rm@gmail.com>
2018-12-15 16:44:24 +0000

    Heroku adaptations

--- script.py
+++ script.py
@@ -4,14 +4,15 @@ import json
 from pprint  import pprint
 import random
 
-def getRandomVerb(a,b):
+def getRandomVerb():
     # We get the json file content (phrasal verbs)
     with open('phrasal_verbs.json') as dataFile:
         phrasalVerbs = json.load(dataFile)
 
     # We get a random position to get the daily phrasal verb
-    for x in range(len(phrasalVerbs)):
-        randomPosition = random.randint(0,len(phrasalVerbs))
+    #for x in range(len(phrasalVerbs)):
+    #    randomPosition = random.randint(0,len(phrasalVerbs))
+    randomPosition = random.randint(0,len(phrasalVerbs))
 
     # Now, we have the choosen item
     print("Content-Type: javascript/json")
